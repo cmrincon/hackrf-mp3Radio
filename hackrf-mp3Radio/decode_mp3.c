d3e9db291bacd36699831eb187fc4b26624e245c
César fumfum <cmrincon611@hotmail.com>
2019-06-09 12:31:20 +0200

    upddate

---
+++ hackrf-mp3Radio/decode_mp3.c
@@ -0,0 +1,12 @@
+#include "decode_mp3.h"
+
+void init_decode(char* file_name)
+{
+
+}
+
+int get_mp3_frame(FILE * fd, char * frame, unsigned short * size)
+{
+	return 0;
+}
+
---
+++ hackrf-mp3Radio/decode_mp3.h
@@ -0,0 +1,5 @@
+#include<libavcodec/avcodec.h>
+#include<libavformat/avformat.h>
+
+void init_decode(char* file_name);
+int get_mp3_frame(FILE *fd,char *frame,unsigned short *size);
