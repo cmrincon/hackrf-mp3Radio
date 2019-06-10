f10d667fb15fe007363491c613e5b55c9f084c0f
César <cmrincon611@hotmail.com>
2019-06-10 14:09:36 +0200

    update

--- hackrf-mp3Radio/decode_mp3.c
+++ hackrf-mp3Radio/decode_mp3.c
@@ -2,11 +2,18 @@
 
 void init_decode(char* file_name)
 {
-
+	
 }
 
 int get_mp3_frame(FILE * fd, char * frame, unsigned short * size)
 {
 	return 0;
+}
+
+unsigned int decode(char * file_name, char * outbuffer, unsigned int bufferlen)
+{
+	unsigned int ret = 0;
+
+	return ret;
 }
 
--- hackrf-mp3Radio/decode_mp3.h
+++ hackrf-mp3Radio/decode_mp3.h
@@ -3,3 +3,4 @@
 
 void init_decode(char* file_name);
 int get_mp3_frame(FILE *fd,char *frame,unsigned short *size);
+unsigned int decode(char *file_name, char *outbuffer, unsigned int bufferlen);
